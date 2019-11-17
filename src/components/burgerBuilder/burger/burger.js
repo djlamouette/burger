b68b05f3f969390baf828736a14fe8bd859e98f4
Marin Marechal <marinmarechal@MacBook-Pro-de-Marin.local>
2019-11-17 17:55:15 +0100

    save

--- src/components/burgerBuilder/burger/burger.js
+++ src/components/burgerBuilder/burger/burger.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
+import './burger.scss'
+
 const burger = ( props ) => {
+
+
+    const ingredients = Object.keys(props.__ingredients).map((ingredient, index) => {
+        return (            
+            [...Array(props.__ingredients[ingredient])].map((_, j) => {
+                return <div className={"ingredient " + ingredient} key={ingredient + j}></div> 
+            })
+        );
+    });
     
     return (
         <div className="burger">
-            <p>meat : {props.__ingredients.meat}</p>
-            <p>cheese : {props.__ingredients.cheese}</p>
-            <p>tomatoes : {props.__ingredients.tomato}</p>
-            <p>bacon : {props.__ingredients.bacon}</p>
+            <div className="BreadTop">
+                <div className="Seeds1"></div>
+                <div className="Seeds2"></div>
+            </div>
+            {ingredients}
+            <div className="BreadBottom"></div>
         </div>
     )
 }
--- src/components/burgerBuilder/burgerBuilder.js
+++ src/components/burgerBuilder/burgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import BuildControls from './buildControls/buildControls'
 import Burger from './burger/burger'
@@ -7,34 +7,46 @@ export class burgerBuilder extends Component {
 
     state = {
         ingredients: {
-            meat: 1,
-            cheese: 1,
-            tomato: 1,
-            bacon: 0
+            Cheese: 1,
+            Bacon: 1,
+            Meat: 1,
+            Salad: 0
         }
     }
 
     increaseIngredientHandler = (ingredient) => {
         if (this.state.ingredients[ingredient] < 5) {
-            this.setState({ingredients[ingredient]: this.state.ingredients[ingredient]++})
+            this.setState(prevState => {
+                let ingredients = {
+                    ...this.state.ingredients
+                }
+                ingredients[ingredient] = prevState.ingredients[ingredient] + 1;
+                return {ingredients}
+            })
         }
-        
     }
 
     decreaseIngredientHandler = (ingredient) => {
-        console.log(ingredient)
+        if (this.state.ingredients[ingredient] > 0 ) {
+            this.setState(prevState => {
+                let ingredients = {
+                    ...this.state.ingredients
+                }
+                ingredients[ingredient] = prevState.ingredients[ingredient] - 1;
+                return {ingredients}
+            })
+        }
     }
-
 
     render() {
         return (
             <div className="burgerBuilder">
-                <Burger __ingredients={this.state.ingredients} />
+                <Burger __ingredients={this.state.ingredients}/>
                 <BuildControls
-                 __ingredients={this.state.ingredients}
-                 __increase={this.increaseIngredientHandler}
-                 __decrease={this.decreaseIngredientHandler}/>
-                
+                    __ingredients={this.state.ingredients}
+                    __increase={this.increaseIngredientHandler}
+                    __decrease={this.decreaseIngredientHandler}/>
+
             </div>
         )
     }
